97b61a1b809048b1c9f93373c033bd245d338a05
Coni26 <onyxia@jupyter-python-952765-0.jupyter-python-952765.user-coni.svc.cluster.local>
2023-02-24 11:31:17 +0000

    model fix

--- src/models.py
+++ src/models.py
@@ -15,7 +15,7 @@ class RNNModel(nn.Module):
         self.hidden_dim = hidden_dim
         self.n_layers = n_layers
 
-        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True)   
+        self.rnn = nn.RNN(input_size, hidden_dim, n_layers, batch_first=True, nonlinearity='tanh', bias=True)   
         self.fc = nn.Linear(hidden_dim, output_size)
     
     def forward(self, x):
@@ -28,6 +28,7 @@ class RNNModel(nn.Module):
         out = out.contiguous().view(-1, self.hidden_dim)
         out = self.fc(out)
         out = F.softmax(out, dim=1)
+        #out = torch.sigmoid(out)
         return out
     
     def init_hidden(self, batch_size):
