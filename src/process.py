1f762d61b1256e3504f50e312b07d5a2d3264ac4
Coni26 <onyxia@jupyter-pytorch-gpu-608799-0.jupyter-pytorch-gpu-608799.user-coni.svc.cluster.local>
2023-03-01 15:03:46 +0000

    add models + test_only

--- src/process.py
+++ src/process.py
@@ -11,23 +11,33 @@ def get_encoders():
     return df
 
 
-def embed_daily_dialog(encoder='all-MiniLM-L6-v2'):
+def embed_daily_dialog(encoder='all-MiniLM-L6-v2', test_only=False):
     model = SentenceTransformer(encoder)
     dataset = load_dataset('daily_dialog','multi-label')  
-    training_dataset = dataset['train'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
-    validation_dataset = dataset['validation'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
-    test_dataset = dataset['test'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
-    training_dataset.save_to_disk("data/daily_dialog_" + encoder + "_train.hf")
-    validation_dataset.save_to_disk("data/daily_dialog_" + encoder + "_validation.hf")
-    test_dataset.save_to_disk("data/daily_dialog_" + encoder + "_test.hf")
+    if test_only:
+        test_dataset = dataset['test'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
+        test_dataset.save_to_disk("data/daily_dialog_" + encoder + "_test.hf")
+    else:
+        training_dataset = dataset['train'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
+        validation_dataset = dataset['validation'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
+        test_dataset = dataset['test'].map(lambda row: {'embeddings': model.encode(row['dialog'])}, batched=False)
+        training_dataset.save_to_disk("data/daily_dialog_" + encoder + "_train.hf")
+        validation_dataset.save_to_disk("data/daily_dialog_" + encoder + "_validation.hf")
+        test_dataset.save_to_disk("data/daily_dialog_" + encoder + "_test.hf")
 
     
 
-def load_data_embedded(encoder='all-MiniLM-L6-v2'):
+def load_data_embedded(encoder='all-MiniLM-L6-v2', test_only=False):
     arr = os.listdir('data/')
-    if not("daily_dialog_" + encoder + "_train.hf" in arr):
-        embed_daily_dialog(encoder)
-    train = load_from_disk("data/daily_dialog_" + encoder + "_train.hf")  
-    validation = load_from_disk("data/daily_dialog_" + encoder + "_validation.hf")  
-    test = load_from_disk("data/daily_dialog_" + encoder + "_test.hf")   
-    return train, validation, test
+    if test_only:
+        if not("daily_dialog_" + encoder + "_train.hf" in arr):
+            embed_daily_dialog(encoder, test_only=test_only)
+        test = load_from_disk("data/daily_dialog_" + encoder + "_test.hf")
+        return None, None, test
+    else:
+        if not("daily_dialog_" + encoder + "_train.hf" in arr):
+            embed_daily_dialog(encoder, test_only=test_only)
+        train = load_from_disk("data/daily_dialog_" + encoder + "_train.hf")  
+        validation = load_from_disk("data/daily_dialog_" + encoder + "_validation.hf")  
+        test = load_from_disk("data/daily_dialog_" + encoder + "_test.hf")   
+        return train, validation, test
